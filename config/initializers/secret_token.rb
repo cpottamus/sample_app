e1e771b8185573971a404e08c1c9bcfe6a7da685
Carson Potter <cpotter@dhcp-10-224-159-246.enova.com>
2013-08-13 19:32:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'cca8b8cab3bbee30e2d4c13372c3944cf5168f936646cfc1cd0a72748a507e7768d269f853279ff2b4273a330175276dc07ecd5058b21da5d167e7f346617ba7'
