534c5553f08bc6c9e29479b5f107c7dbff103140
Carson Potter <cpotter@cpotter.local>
2013-08-18 12:50:42 -0500

    Chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
-  match '/contact' :to => 'pages#contact'
-  match '/about' :to => 'pages#about'
-  match '/help' :to => 'pages#help'
+  match '/contact' => 'pages#contact'
+  match '/about' => 'pages#about'
+  match '/help' => 'pages#help'
   root :to => 'pages#home'
   
 
